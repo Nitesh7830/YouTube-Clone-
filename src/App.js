4d1f920ea7fdb27815a1204be53c26356f89a322
Nitesh@7830 <Nitesh7830>
2024-03-01 23:04:41 +0530

    setup project && add tailwind in it

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-slate-950 text-white'>
+        Hello
+      </h1>
+    </div>
+  );
+}
+
+export default App;
